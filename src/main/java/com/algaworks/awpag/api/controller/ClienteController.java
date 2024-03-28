0ab9c4608308e21b5674a2f91bab9dbcb0c5fb5b
Diego Santana <3c.santana@gmail.com>
2024-03-28 08:58:27 -0300

    refactor: altera a lógica movendo as operações para o repository

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -1,21 +1,23 @@
 package com.algaworks.awpag.api.controller;
 
 import com.algaworks.awpag.domain.model.Cliente;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import com.algaworks.awpag.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
-@RestController
-public class ClienteController {
-    @PersistenceContext
-    private EntityManager manager;
-    
-    @GetMapping("/clientes")
-    public List<Cliente> listar(){
-        return manager.createQuery("from Cliente", Cliente.class).getResultList();
+    @AllArgsConstructor
+    @RestController
+    public class ClienteController {
+        
+        private final ClienteRepository clienteRepository;
+        @GetMapping("/clientes")
+        public List<Cliente> listar(){
+//            return clienteRepository.findByNomeContaining("Bob");
+//            return clienteRepository.findByNome("Bob Brown");
+            return clienteRepository.findAll();
+        }
     }
-}
+
