de91da7a95a95ecca976322a8baedc38ef583f26
Diego Santana <3c.santana@gmail.com>
2024-03-29 20:04:36 -0300

    feat: add validação na classe de domínio com o Bean validation

--- src/main/java/com/algaworks/awpag/domain/model/Cliente.java
+++ src/main/java/com/algaworks/awpag/domain/model/Cliente.java
@@ -1,32 +1,34 @@
 package com.algaworks.awpag.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-//@Table(name = "tb_cliente")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
-
 public class Cliente { 
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+    @NotBlank
+    @Size(max = 255)
+    @Email
     private String email;
+    
     @Column(name = "fone")
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
 
-    public Cliente(String nome, String email, String telefone) {
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
-    }
-
-    public Cliente() {
-    }
 }
