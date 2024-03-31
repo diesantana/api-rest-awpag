54235e6046d562432da59cf7c3f0a1405a192f01
Diego Santana <3c.santana@gmail.com>
2024-03-31 18:01:25 -0300

    feat: criação do service ParcelamentoService

---
+++ src/main/java/com/algaworks/awpag/domain/services/ParcelamentoService.java
@@ -0,0 +1,38 @@
+package com.algaworks.awpag.domain.services;
+
+import com.algaworks.awpag.domain.exception.DomainException;
+import com.algaworks.awpag.domain.model.Cliente;
+import com.algaworks.awpag.domain.model.Parcelamento;
+import com.algaworks.awpag.domain.repository.ClienteRepository;
+import com.algaworks.awpag.domain.repository.ParcelamentoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Service
+public class ParcelamentoService {
+    private final ParcelamentoRepository parcelamentoRepository;
+    private final CadastroClienteService clienteService;
+    
+    @Transactional
+    public Parcelamento cadastrar(Parcelamento novoParcelemto) {
+        // verifica se o usuário está passando um id na requisição
+        if(novoParcelemto.getId() != null) {
+            throw new DomainException("Parcelamento a ser criado não deve possuir Id");
+        }
+        
+        //verifica se o cliente existe, para associar o parcelamento a ele.
+        Cliente cliente = clienteService.buscar(novoParcelemto.getCliente().getId());
+        
+        // carrega os dados do cliente existente, para exibir no resposta da requisição
+        novoParcelemto.setCliente(cliente);
+        
+        // add a data de criação
+        novoParcelemto.setDataCriacao(LocalDateTime.now());
+        
+         return parcelamentoRepository.save(novoParcelemto);
+    }
+}
