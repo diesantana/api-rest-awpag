0407eb6b6877c3554123874639faaedf7c70248c
Diego Santana <3c.santana@gmail.com>
2024-03-31 18:00:20 -0300

    feat: criação da classe de dominio parcelamento

---
+++ src/main/java/com/algaworks/awpag/domain/model/Parcelamento.java
@@ -0,0 +1,46 @@
+package com.algaworks.awpag.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity // Espeficia que a classe é uma entidade 
+@Getter // Gera os getters 
+@Setter // setters 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // Comapração é feito somento com o atributo que estiver include
+public class Parcelamento {
+    
+    @Id                 // indetificador da classe (PK)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Indica que o valor deve ser fornecido auto pelo bd
+    @EqualsAndHashCode.Include  // define esse atributo na comparação no equals e hashCode
+    private Long id;
+    
+    @Valid
+    @NotNull
+    @ManyToOne
+    private Cliente cliente;
+    
+    @NotNull
+    @Size(max = 20)
+    private String descricao;
+    
+    @NotNull
+    @Positive
+    private BigDecimal valorTotal;
+    
+    @NotNull
+    @Positive
+    @Max(12)
+    private Integer quantidadeParcelas;
+    private LocalDateTime dataCriacao;
+    
+}
