e2ad5237815f29514eca64ad41878365727ae873
Diego Santana <3c.santana@gmail.com>
2024-04-01 07:56:22 -0300

    transformação de obj com ModelMapper

--- src/main/java/com/algaworks/awpag/api/controller/ParcelamentoController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ParcelamentoController.java
@@ -1,12 +1,15 @@
 package com.algaworks.awpag.api.controller;
 
+import com.algaworks.awpag.api.assembler.ParcelamentoAssembler;
 import com.algaworks.awpag.api.model.ParcelamentoModel;
+import com.algaworks.awpag.api.model.input.ParcelamentoInput;
 import com.algaworks.awpag.domain.exception.DomainException;
 import com.algaworks.awpag.domain.model.Parcelamento;
 import com.algaworks.awpag.domain.repository.ParcelamentoRepository;
 import com.algaworks.awpag.domain.services.ParcelamentoService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,32 +22,27 @@ import java.util.List;
 public class ParcelamentoController {
     private final ParcelamentoRepository parcelamentoRepository;
     private final ParcelamentoService parcelamentoService;
+    private final ParcelamentoAssembler parcelamentoAssembler;
     
     @GetMapping
-    public List<Parcelamento> lisar() {
-        return parcelamentoRepository.findAll();
+    public List<ParcelamentoModel> lisar() {
+        return parcelamentoAssembler.toCollectionModel(parcelamentoRepository.findAll());
     }
     
     @GetMapping("/{parcelamentoId}")
     public ResponseEntity<ParcelamentoModel> buscar(@PathVariable Long parcelamentoId) {
         return parcelamentoRepository.findById(parcelamentoId)
-                .map(parcelamentoEntity -> {
-                        var parcelamentoModel = new ParcelamentoModel();
-                        parcelamentoModel.setId(parcelamentoEntity.getId());
-                        parcelamentoModel.setNomeCliente(parcelamentoEntity.getCliente().getNome());
-                        parcelamentoModel.setDescricao(parcelamentoEntity.getDescricao());
-                        parcelamentoModel.setValorTotal(parcelamentoEntity.getValorTotal());
-                        parcelamentoModel.setParcelas(parcelamentoEntity.getQuantidadeParcelas());
-                        parcelamentoModel.setDataCriacao(parcelamentoEntity.getDataCriacao());
-                        
-                        return ResponseEntity.ok(parcelamentoModel);
-                })
+                .map(parcelamentoAssembler::toModel)
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build()); 
     }
     
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Parcelamento cadastrar(@Valid @RequestBody Parcelamento parcelamento) {
-        return parcelamentoService.cadastrar(parcelamento);
+    public ParcelamentoModel cadastrar(@Valid @RequestBody ParcelamentoInput parcelamentoInput) {
+        Parcelamento novoParcelamento = parcelamentoAssembler.toEntity(parcelamentoInput);
+        
+        Parcelamento parcelamentoCadastrado = parcelamentoService.cadastrar(novoParcelamento);
+        return parcelamentoAssembler.toModel(parcelamentoCadastrado);
     }
 }
